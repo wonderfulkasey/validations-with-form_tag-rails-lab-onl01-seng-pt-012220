b59445806ab6812c343b5189a81da4018301210c
Kasey Arnold <kaseyarnold15@gmail.com>
2020-06-07 18:40:31 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -9,11 +9,25 @@ class AuthorsController < ApplicationController
 
   def create
     @author = Author.new(author_params)
-    if @author.valid?
-      @author.save
+
+    if @author.save
       redirect_to author_path(@author)
     else
       render :new
+    end
+  end
+
+  def edit
+    @author = Author.find(params[:id])
+  end
+
+  def update
+    @author = Author.find(params[:id])
+
+    if @author.update(author_params)
+      redirect_to author_path(@author)
+    else
+      render :edit
     end
   end
 
